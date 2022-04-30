e5b40e49738eb0adea774b035729bf7c26d33f6c
Anderson Souza <andersonszdc@gmail.com>
2022-04-30 14:06:39 -0300

    styled sign-up page

--- frontend/src/pages/sign-up/index.tsx
+++ frontend/src/pages/sign-up/index.tsx
@@ -6,24 +6,22 @@ import { SignupWrapper } from "./styles";
 const SignUp = () => {
   return (
     <SignupWrapper>
-      <div>
-        <p>start for free</p>
-        <h1>
-          Create new account<span>.</span>
-        </h1>
-        <h2>
-          Already A Member? <span>Log in</span>
-        </h2>
-        <form>
-          <div className="full-name">
-            <Input label="First Name" />
-            <Input label="Last Name" />
-          </div>
-          <Input label="Email" />
-          <Input label="Password" />
-          <Button text="Create account" />
-        </form>
-      </div>
+      <p className="notification">start for free</p>
+      <h1 className="title">
+        Create new account<span className="dot">.</span>
+      </h1>
+      <h2 className="subtitle">
+        Already A Member? <span className="to-signin">Log in</span>
+      </h2>
+      <form className="form">
+        <div className="full-name">
+          <Input label="First Name" />
+          <Input label="Last Name" />
+        </div>
+        <Input label="Email" />
+        <Input label="Password" />
+        <Button text="Create account" />
+      </form>
     </SignupWrapper>
   );
 };
--- frontend/src/pages/sign-up/styles.ts
+++ frontend/src/pages/sign-up/styles.ts
@@ -1,3 +1,46 @@
 import styled from "styled-components";
 
-export const SignupWrapper = styled.div``
+export const SignupWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .notification {
+    font-size: 16px;
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+
+  .title {
+    font-size: 50px;
+    font-weight: 700;
+    margin: 24px 0;
+  }
+
+  .subtitle {
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  .dot,
+  .to-signin {
+    color: #16d3e4;
+  }
+
+  .to-signin {
+    cursor: pointer;
+  }
+
+  .form {
+    margin-top: 48px;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+  }
+
+  .full-name {
+    display: flex;
+    gap: 24px;
+  }
+`;
